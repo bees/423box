b3fd29dfefa0a6534eff5c1164a311c3dda6d7d6
arian <daneshv2@illinois.edu>
2016-02-29 11:00:36 -0600

    booting into 3.19, tags generating correctly

--- scripts/base.sh
+++ scripts/base.sh
@@ -22,3 +22,15 @@ GRUB_CMDLINE_LINUX="debian-installer=en_US"
 EOF
 
 update-grub
+
+cat <<EOF > /etc/default/grub
+# If you change this file, run 'update-grub' afterwards to update
+# /boot/grub/grub.cfg.
+
+GRUB_TIMEOUT="1>2"
+GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
+GRUB_CMDLINE_LINUX_DEFAULT="quiet"
+GRUB_CMDLINE_LINUX="debian-installer=en_US"
+EOF
+
+update-grub
--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -8,3 +8,6 @@ mkdir -pm 700 /home/vagrant/.ssh
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
+
+# Set motd
+echo '423 Development Environment\nConfiguration source files available at bees/423box\nAny config issues are considered a bug, submit a report or pull request\n' > /etc/motd
